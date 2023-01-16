249b367c996bfa8cfd5141706349d5da6e925ce2
Admins@ANIL.(none) <tjadhav911@gmail.com>
2023-01-16 20:14:17 +0530

    Encapsulation

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,7 +2,7 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        RWOnly a=new RWOnly("ram");
+        RWOnly a=new RWOnly();
         //Cannot resolve method 'RWOnly' in 'RWOnly'
         a.setName("ganesh");
         a.getName();
